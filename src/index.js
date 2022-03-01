a307b65119542302bab4747333d226f9bbe6bcc5
huseyinbatta <huseyinbattal42@gmail.com>
2022-03-01 18:40:57 +0300

    React-Redux-Sample

--- src/index.js
+++ src/index.js
@@ -3,6 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore,combineReducers} from 'redux'
+
+function myReducer(state,action){
+  //console.log("State:",state," Action:",action)
+  if (action.type === 'changeTheState') {
+    return action.payload.newState
+  } 
+  return "State Text"}
+
+  function userReducer(state="",action){ return state  }  
+  function productReducer(state=[],action){return state  }
+
+  const rootReducer = combineReducers(
+    { userReducer, productReducer, myReducer });
+
+const myStore = createStore(rootReducer,
+  {
+  productReducer:[{name:'Sony',type:'Mp3 Player'}],
+  myReducer:'First Text'}
+  );//Create your store
+//console.log("Old State:",myStore.getState())
+
+const action={
+  type:'changeTheState',//uniqe Id
+  payload:{newState:'My new State'}
+}
+const action2={
+  type:'changeTheState',//uniqe Id
+  payload:{newState:'My new State 2'}
+}
+
+myStore.subscribe(()=>{
+  //console.log("subscribe runned.-Store Updated.",myStore.getState())
+})
+
+myStore.dispatch(action)
+//console.log("Last State:",myStore.getState())
+
+myStore.dispatch(action2)
+console.log("Last State:",myStore.getState())
+
+
 
 ReactDOM.render(
   <React.StrictMode>
